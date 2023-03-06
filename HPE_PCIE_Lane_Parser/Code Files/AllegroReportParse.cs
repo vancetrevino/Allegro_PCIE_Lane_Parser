2bce48ae49c701cdf435b194cf8d2f4f22032048
Vance Trevino <trevino.vance@gmail.com>
2023-03-06 17:18:55 -0600

    Removed "Riser" tab from main windown. Will add in future release.

--- HPE_PCIE_Lane_Parser/Code Files/AllegroReportParse.cs
+++ HPE_PCIE_Lane_Parser/Code Files/AllegroReportParse.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿
 using Allegro_PCIE_Lane_Parser.Class_Files;
 
 
